1cd5b84966883606bae98acbb8e19ba300edb6dd
Tobias Guse <tobias.guse@mogul.gg>
2019-10-15 20:34:35 +0200

    add basic menu

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { getPopularRepos } from '../utils/api'
 
 const Container = styled.nav`
   display:flex;
@@ -11,17 +12,38 @@ const NavButton = styled.button`
   border:none;
   background:transparent;
   font-size:16px;
+  color:${props => props.active ? 'tomato' : 'black'};
   font-weight:600;
   cursor:pointer;
+  :focus {
+    outline:none;
+  }
   :hover {
-    color:Rgba(0,0,0,0.75);
+    color:${props => props.active ? 'tomato' : 'rgba(0,0,0,0.75)'};
   }
 `
 export default class Popular extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  updateLanguage (selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+  }
+
   render () {
+    const { selectedLanguage } = this.state
     const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
 
-    const MenuItems = languages.map(lang => <NavButton key={lang}>{lang}</NavButton>)
+    const MenuItems = languages.map(lang => <NavButton key={lang} onClick={() => this.updateLanguage(lang)} active={selectedLanguage === lang}>{lang}</NavButton>)
 
     return (
       <Container>
